53d4f076001da250322ccbd8fc09634011f71d88
Alejandro Upegui Betancur <aub.17@hotmail.com>
2021-01-26 21:01:01 -0500

    Initial commit

---
+++ src/components/atomic/atoms/Button/Button.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const Button = () =><button>Hola!!</button>
+
+export default Button;
+//Button.displayName = 'Button'
---
+++ src/components/stories/atomic/atoms/Button.stories.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Button from '../../../atomic/atoms/Button/Button';
+export default {
+    title: 'Atoms/Button',
+    component : Button
+}
+
+
+export const Primary = () => <Button/>
+
+//Primary.storyName = 'Primary'
