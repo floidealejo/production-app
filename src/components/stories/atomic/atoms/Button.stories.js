0cddeb544e5c22004e2459cbc1292d58bfb4295e
Alejandro Upegui Betancur <aub.17@hotmail.com>
2021-02-14 18:38:39 -0500

    [ADD] Using our custom properties with Design Tokens

--- src/components/stories/atomic/atoms/Button.stories.js
+++ src/components/stories/atomic/atoms/Button.stories.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import Button from '../../../atoms/Button/Button';
+import Icon from "../../../atoms/Icon/Icon"
+
 export default {
     title: 'Atoms/Button',
     component : Button
-}
+};
 
 
 export const Primary = () => <Button type="primary">Button primary</Button>
 export const Secondary = () => <Button type="secondary">Button secondary</Button>
+export const WithIcon = () => <Button type="secondary">Button secondary<Icon type="right-arrow"/></Button>
 export const Tertiary = () => <Button type="tertiary">Button tertiary</Button>
 
 //Primary.storyName = 'Primary'
---
+++ src/components/stories/atomic/atoms/Icon.stories.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Icon from '../../../atoms/Icon/Icon';
+export default {
+    title: 'Atoms/Icon',
+    component : Icon
+}
+
+
+export const Default = () => <Icon type="right-arrow">Button primary</Icon>
+// export const Secondary = () => <Button type="secondary">Button secondary</Button>
+// export const Tertiary = () => <Button type="tertiary">Button tertiary</Button>
+
+
